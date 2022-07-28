66f360d943d419817cb885ec3ba93d22752006ea
base1551 <baseball.1.kyoto@gmail.com>
2022-07-28 11:46:44 +0900

    マルチ認証実装

---
+++ app/Models/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use HasFactory, Notifiable;
+    protected $guard = 'admins';
+    protected $fillable = [
+        'name',
+        'tel',
+        'email',
+        'password',
+    ];
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::prefix('admin')->name('admin.')->group(function () {
+    require __DIR__ . '/admin.php';
+});
+
+require __DIR__ . '/auth.php';
